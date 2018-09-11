5de0d9a8a9e11d74ba7bea43b575ae91ce60dda8
Wan Cheul Kim <wckim88@gmail.com>
2018-09-10 22:04:37 -0700

    Online user gets indication on direct message list (#10)

--- app/scripts/services/User.js
+++ app/scripts/services/User.js
@@ -2,6 +2,7 @@
   function User($firebaseArray, $firebaseObject) {
     var User = {};
     var userRef = firebase.database().ref('users');
+    var connectedRef = firebase.database().ref('.info/connected');
     var users = $firebaseArray(userRef);
 
     User.all = users;
@@ -16,6 +17,19 @@
 
     User.getGravatar = function(uid) {
       return '//www.gravatar.com/avatar/' + users.$getRecord(uid).emailHash;
+    };
+
+    User.setOnline = function(uid) {
+      var connected = $firebaseObject(connectedRef);
+      var online = $firebaseArray(userRef.child(uid+'/online'));
+
+      connected.$watch(function() {
+        if(connected.$value) {
+          online.$add(true).then(function(connectedRef) {
+            connectedRef.onDisconnect().remove();
+          });
+        }
+      });
     };
 
     return User;
